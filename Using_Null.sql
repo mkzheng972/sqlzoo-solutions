d8de02c51c5c4aacb8b37fc54a4cd0a9c0c874a2
Mark <58439812+mkzheng972@users.noreply.github.com>
2020-12-18 17:43:02 -0500

    Update Using_Null.sql

--- Using_Null.sql
+++ Using_Null.sql
@@ -3,3 +3,50 @@
 SELECT name
 FROM teacher
 WHERE dept IS NULL
+
+#2
+
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+           
+#3
+
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+           
+#4
+
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
+           
+#5
+
+SELECT teacher.name, COALESCE(teacher.mobile, '07986 444 2266')
+FROM teacher
+
+#6
+
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON teacher.dept = dept.id
+
+#7
+
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
+
+#8
+
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON teacher.dept = dept.id
+GROUP BY dept.name
+
+#9
+
+SELECT teacher.name,
+CASE WHEN teacher.dept = 1 OR teacher.dept = 2 THEN 'Sci' ELSE 'Art' END
+FROM teacher
+
+
